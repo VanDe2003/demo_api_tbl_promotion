834f1ae72dc980f89e9cb5ab80841ac17549ae7d
VanDe2003 <phanvande2003.qt@gmail.com>
2023-07-25 11:33:39 +0700

    update delete

---
+++ process_delete_promotion.php
@@ -0,0 +1,40 @@
+<?php
+
+if (isset($_GET["promotion_id"])) {
+	$promotion_id = $_GET["promotion_id"];
+
+	// Kiểm tra nếu promotion_id không hợp lệ (không phải là chuỗi), hiển thị thông báo lỗi
+	if (!is_string($promotion_id)) {
+		echo "Lỗi: promotion_id không hợp lệ.";
+		exit();
+	}
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => 'http://localhost:3000/api/habeauty/promotion/' . $promotion_id,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'DELETE',
+		CURLOPT_HTTPHEADER => array(
+			'Content-Type: application/json',
+		),
+	));
+
+	$response = curl_exec($curl);
+
+	curl_close($curl);
+
+	// Hiển thị phản hồi từ yêu cầu DELETE
+	echo $response;
+
+	header("Location: table_habeauty.php"); // Chuyển hướng sau khi xóa thành công
+	exit();
+} else {
+	echo "Lỗi: Không có promotion_id được cung cấp.";
+	exit();
+}
--- table_habeauty.php
+++ table_habeauty.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-  <div class="container">
-    <h2>Promotion Data</h2>
-    <!-- Thêm nút để chuyển hướng đến trang thêm khuyến mãi -->
-    <a href="add_promotion.php" class="btn btn-primary">Thêm khuyến mãi</a>
-    <!-- Kết thúc phần thêm nút -->
+    <div class="container">
+        <h2>Promotion Data</h2>
+        <!-- Thêm nút để chuyển hướng đến trang thêm khuyến mãi -->
+        <a href="add_promotion.php" class="btn btn-primary">Thêm khuyến mãi</a>
+        <!-- Kết thúc phần thêm nút -->
 
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Start Time</th>
-          <th>End Time</th>
-          <th>Describe</th>
-          <th>Status</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Describe</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+// Kết nối đến cơ sở dữ liệu (sử dụng thông tin kết nối của bạn)
 $servername = "localhost";
 $username = "root";
 $password = "mysql";
@@ -47,8 +48,10 @@ if ($result->num_rows > 0) {
 		echo "<td>" . $row["promotion_end_time"] . "</td>";
 		echo "<td>" . $row["promotion_describe"] . "</td>";
 		echo "<td>" . $row["promotion_status"] . "</td>";
-		echo "<td><a href='edit_promotion.php?promotion_id=" . $row["promotion_id"] . "&promotion_status=" . $row["promotion_status"] . "' class='btn btn-primary'>Sửa</a></td>";
-
+		echo "<td>
+                            <a href='edit_promotion.php?promotion_id=" . $row["promotion_id"] . "&promotion_status=" . $row["promotion_status"] . "' class='btn btn-primary'>Sửa</a>
+                            <a href='process_delete_promotion.php?promotion_id=" . $row["promotion_id"] . "' class='btn btn-danger' onclick=\"return confirm('Bạn có chắc chắn muốn xóa khuyến mãi này?')\">Xóa</a>
+                            </td>";
 		echo "</tr>";
 	}
 } else {
@@ -57,8 +60,8 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
-      </tbody>
-    </table>
-  </div>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
